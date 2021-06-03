17c4364b9c76b72c12c7e5919adfc62e3c7ef94b
TarunStark <TarunStark@users.noreply.github.com>
2021-06-03 10:48:25 +0530

    commit

---
+++ dfsgraph.py
@@ -0,0 +1,31 @@
+from _collections import defaultdict
+
+class Graph:
+
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    def addEdge(self,u,v):
+        self.graph[u].append(v)
+
+    def dfsutil(self,v,visited):
+        visited[v]=True
+        print(v,end=' ')
+        for i in self.graph[v]:
+            if not visited[i]:
+                self.dfsutil(i,visited)
+
+    def dfs(self,s):
+        visited     = [False]*(len(self.graph))
+        self.dfsutil(s,visited)
+
+g=Graph()
+
+g.addEdge(0, 1)
+g.addEdge(0, 2)
+g.addEdge(1, 2)
+g.addEdge(2, 0)
+g.addEdge(2, 3)
+g.addEdge(3, 3)
+
+g.dfs(2)
